2f18087abddd88e5f5064c1b0c7b7c63574207aa
Robert Allaway <allaway@users.noreply.github.com>
2021-03-23 09:12:19 -0700

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -1,25 +1,17 @@
 
-db <- readRDS("Data/drug_target_associations_v2.rds")
+db <- readRDS("Data/drug_target_associations_v4.rds")
 
+db.names <- readRDS("Data/distinct_compound_names_v4.rds")
+db$inchikey <- as.character(db$inchikey)
 
-db.names <- readRDS("Data/distinct_compound_names.rds")
-db$internal_id <- as.character(db$internal_id)
+fp.extended <- readRDS("Data/db_fingerprints_extended_v4.rds")
 
-fp.extended <- readRDS("Data/db_fingerprints_extended.rds")
+fp.circular <- readRDS("Data/db_fingerprints_circular_v4.rds")
 
-fp.circular <- readRDS("Data/db_fingerprints_circular.rds")
+fp.maccs <- readRDS("Data/db_fingerprints_maccs_v4.rds")
 
-# fp.kr <- readRDS(synGet("syn11898431")$path)
-# fp.kr <- fp.kr[names(fp.kr) %in% unique(db$internal_id)]
-
-fp.maccs <- readRDS("Data/db_fingerprints_maccs.rds")
-
-# fp.pubchem <- readRDS(synGet("syn12031332")$path)
-# fp.pubchem <- fp.pubchem[names(fp.pubchem) %in% unique(db$internal_id)]
-
-
-db.links <- read.table("Data/db_external_links.txt", sep = "\t", header = T)
-db.gene.links <- read.table("Data/gene_external_links.txt", sep = "\t", header = T)
+db.links <- read.table("Data/db_external_links_v4.txt", sep = "\t", header = T)
+db.gene.links <- read.table("Data/gene_external_links_v4.txt", sep = "\t", header = T)
 db.genes <- db.gene.links$hugo_gene
 
 ##get CTRP data for heatmap
