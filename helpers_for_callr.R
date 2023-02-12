94337d1d74607fc71af7354312a1ba01d88be668
bhoff <bruce.hoff@sagebase.org>
2023-02-11 19:48:27 -0800

    added db to helpers_for_callr.R

--- helpers_for_callr.R
+++ helpers_for_callr.R
@@ -7,3 +7,17 @@ fp.extended <- readRDS("Data/db_fingerprints_extended_v4.rds")
 fp.circular <- readRDS("Data/db_fingerprints_circular_v4.rds")
 
 fp.maccs <- readRDS("Data/db_fingerprints_maccs_v4.rds")
+
+
+#----- repeated from helpers.R ---------
+db <- readRDS("Data/compound_target_associations_v4.rds")
+
+db.names <- readRDS("Data/distinct_compound_synonyms_v4.rds")
+
+db$inchikey <- as.character(db$inchikey)
+
+db <- select(db.names, inchikey, pref_name) %>% 
+  distinct() %>% 
+  full_join(db)
+
+#---------------------------------------------------
